0520eb944f917ab155b66a95662ccc8e4c2774df
Alex <a.khatuntsev2015@gmail.com>
2019-09-17 23:32:39 +0300

    Updates

---
+++ precache-manifest.81f3f79c472d2edecb1531bbc162b2fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbe7523d409e7f47371249b425f6ace",
+    "url": "/Test-page/index.html"
+  },
+  {
+    "revision": "fdc638f87b14575b3589",
+    "url": "/Test-page/static/css/main.438f28b8.chunk.css"
+  },
+  {
+    "revision": "5875f0a4cc16a8ce23c6",
+    "url": "/Test-page/static/js/3.fccf3a6d.chunk.js"
+  },
+  {
+    "revision": "5bddedd994df7609e34d",
+    "url": "/Test-page/static/js/4.209dd81b.chunk.js"
+  },
+  {
+    "revision": "0ff8dbf4e663f77d246c",
+    "url": "/Test-page/static/js/5.49cce3ea.chunk.js"
+  },
+  {
+    "revision": "db3717180f88fffa7321",
+    "url": "/Test-page/static/js/6.69e0e71d.chunk.js"
+  },
+  {
+    "revision": "fdc638f87b14575b3589",
+    "url": "/Test-page/static/js/main.4dd5b35f.chunk.js"
+  },
+  {
+    "revision": "c55733352b7d17cc4103",
+    "url": "/Test-page/static/js/runtime~main.236aef5b.js"
+  },
+  {
+    "revision": "1d5c1fd49347532c0f8a",
+    "url": "/Test-page/static/js/xlsx.b929653f.chunk.js"
+  },
+  {
+    "revision": "4e756723f4795bfadbf0c8442b16bc59",
+    "url": "/Test-page/static/media/screen1.4e756723.png"
+  }
+]);
---
+++ static/js/xlsx.b929653f.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonptest-page"]=window["webpackJsonptest-page"]||[]).push([[2],{187:function(n,o){},196:function(n,o){},197:function(n,o){}}]);
+//# sourceMappingURL=xlsx.b929653f.chunk.js.map
